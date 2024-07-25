29b7c475b5222ed34532f8fa4244218cfa8f16cc
Runzhong LI <858438680@qq.com>
2024-07-26 03:38:00 +0800

    Feat: Implement Mamba Block

--- src/LeroMamba/lero_model.py
+++ src/LeroMamba/lero_model.py
@@ -3,29 +3,61 @@ import torch
 from torch import nn
 from mamba_ssm import TreeMamba, BatchedTree
 
+class GatedFeedForward(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, 2 * hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, input_dim)
+        self.activation = nn.SiLU()
+
+    def forward(self, x: BatchedTree) -> BatchedTree:
+        y = self.fc1(x)
+        y, gate = y.chunk(2, dim=-1)
+        y = y * self.activation(gate)
+        y = self.fc2(y)
+        return y
+
+class MambaBlock(nn.Module):
+    def __init__(self, d_model, has_ff=True, d_state=16, expand=2):
+        super().__init__()
+        self.has_ff = has_ff
+        self.norm1 = nn.LayerNorm(d_model)
+        self.mamba = TreeMamba(d_model=d_model, d_state=d_state, expand=expand)
+        if self.has_ff:
+            self.norm2 = nn.LayerNorm(d_model)
+            self.ff = GatedFeedForward(d_model, expand * d_model)
+
+    def forward(self, batched_trees: BatchedTree):
+        x = batched_trees.x
+        batched_trees.x = self.norm1(x)
+        y = self.mamba(batched_trees)
+        x = y + x
+        if self.has_ff:
+            y = self.norm2(x)
+            y = self.ff(y)
+            x = y + x
+        batched_trees.x = x
+        return batched_trees
+
 class LeroNet(torch.nn.Module):
     def __init__(self, input_feature_dim: int) -> None:
         super().__init__()
         self.input_feature_dim = input_feature_dim
-        input_rank = 12
-        num_layer = 1
-        dim = 64
-        state = 64
-        self.low_rank_proj = torch.nn.Linear(input_feature_dim, input_rank)
-        self.feature_proj = torch.nn.Linear(input_rank, dim)
-        self.layer_norm = nn.LayerNorm(dim)
-        self.mamba_layers = nn.ModuleList([TreeMamba(d_model=dim, d_state=state, expand=2) for _ in range(num_layer)])
-        self.batch_norm = nn.BatchNorm1d(dim)
-        self.cost_proj = torch.nn.Linear(dim, dim)
+        num_layer = 2
+        self.dim = 64
+        self.feature_proj = torch.nn.Linear(input_feature_dim, self.dim)
+        self.mamba_layers = nn.ModuleList([MambaBlock(self.dim, has_ff=i != num_layer - 1) for i in range(num_layer)])
+        self.batch_norm = nn.BatchNorm1d(self.dim)
+        self.cost_proj = torch.nn.Linear(self.dim, self.dim)
         self.output_act = nn.LeakyReLU()
-        self.cost_est = nn.Linear(dim, 1)
+        self.cost_est = nn.Linear(self.dim, 1)
 
-    def forward(self, batched_tree: BatchedTree) -> torch.Tensor:
-        x = self.feature_proj(self.low_rank_proj(batched_tree.x))
-        batched_tree.x = self.layer_norm(x)
+    def forward(self, batched_trees: BatchedTree) -> torch.Tensor:
+        batched_trees.x = self.feature_proj(batched_trees.x)
         for m in self.mamba_layers:
-            y = m(batched_tree)
-            batched_tree.x = y
-        y = self.batch_norm(y[:,0,:])
+            batched_trees = m(batched_trees)
+        y = batched_trees.x
+        y = y[:,0,:]
+        y = self.batch_norm(y)
         y = self.output_act(self.cost_proj(y))
         return self.cost_est(y)
